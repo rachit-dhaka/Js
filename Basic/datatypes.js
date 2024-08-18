acb816f3a32d468544d3f03ef33104510a02eac4
Rachit Dhaka <121350166+rachit-dhaka@users.noreply.github.com>
2024-08-18 11:06:27 +0000

    hoo gaya

---
+++ Basic/datatypes.js
@@ -0,0 +1,26 @@
+// alert(3*3*3) we are using node js , not using browser
+
+// Data types in JS 
+/*
+numbers -> 2 to power 53
+string -> " should be written in double quote "
+bool -> ture , false
+null -> value khali hai 
+undefined -> value not defined 
+symbol -> unique 
+
+
+
+
+object 
+*/
+
+
+let name ="rachit "
+let id="2909"
+let age =19
+
+console.log(typeof age);
+console.log(typeof undefined);
+
+
