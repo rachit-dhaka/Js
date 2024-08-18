621f0625eb91fc96d4abc51c444fdfa33fd6ac12
Rachit Dhaka <121350166+rachit-dhaka@users.noreply.github.com>
2024-08-18 10:16:10 +0000

    Badiya

---
+++ Basic/variables.js
@@ -0,0 +1,9 @@
+console.log("rachit dhaka")
+
+const name ="Rachit";
+let RegId="RD23";
+let passion="Coding";
+let state ="Rajasthan";
+let city="Jaipur ";
+
+console.table([name,RegId,passion,state,city]);
