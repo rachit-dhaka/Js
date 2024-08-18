58c039b482410b0dbbe03a48181ba0b757259de4
Rachit Dhaka <121350166+rachit-dhaka@users.noreply.github.com>
2024-08-18 11:23:37 +0000

    conversion & operations

---
+++ Basic/conversion&Operations.js
@@ -0,0 +1,53 @@
+let numbers="2909"
+let naam ="rachit"
+let rollno=23
+// console.log(typeof rollno);
+// console.log(rollno);
+
+
+// console.log(numbers);
+// console.log(typeof numbers);
+
+// let convertingNumberintoString=String(rollno)
+// console.log(convertingNumberintoString);
+// console.log(typeof convertingNumberintoString);
+
+// console.log(typeof numbers);
+// let convertingStringintoNumber=Number(numbers);
+// console.log(convertingStringintoNumber);
+// console.log(typeof convertingStringintoNumber);
+
+
+let check=1;
+console.log(typeof check);
+console.log(check);
+
+let bool=Boolean(check);
+console.log(bool);
+console.log(typeof bool);
+
+
+
+
+
+
+
+
+
+// Notes
+/*
+converting string into number: 
+"33" -> 33
+"Rachit" -> NaN
+
+true -> 1
+false -> 0
+
+
+ */
+
+
+
+
+
+
