47d82f80d0a7db23e76ef19f16ddfeb351947728
Rachit Dhaka <121350166+rachit-dhaka@users.noreply.github.com>
2024-08-18 10:30:36 +0000

    kardiya

--- Basic/variables.js
+++ Basic/variables.js
@@ -5,8 +5,14 @@ let RegId="RD23";
 let passion="Coding";
 let state ="Rajasthan";
 let city="Jaipur ";
+let pincode="2909"
+let verginity;
+console.table([name,RegId,passion,state,city,pincode,verginity]);
 
-console.table([name,RegId,passion,state,city]);
+
+
+
+
 
 
 
