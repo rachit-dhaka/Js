ba258d72ac3e277bafcd092f83e8309c4ef3e7f6
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-08-19 19:50:31 +0530

    hoo gaya

--- Basic/Datatype-summary.js
+++ Basic/Datatype-summary.js
@@ -34,4 +34,31 @@ const myFunctions=function(){
     
 }
 
-console.log(typeof myFunctions);
+// ------------------------------------------------Memory notes -------------------------------------------
+// stack memory (primitive ) and heap memory (non primitive )
+// stack memory -> copy create ki jathi hai 
+// head memory -> pass by refrence -> direct value me change kiya jatha hai 
+
+let myname="rachit"
+let myotherName=myname
+myotherName="chai or cp"
+
+// console.log(myname);
+// console.log(myotherName);
+
+
+
+
+
+// fuction me sidha original value me change hotha hai 
+let user={
+    email:"rachitdhaka29@gmail.com",
+    contact:"9176647509",
+}
+let user2=user
+user2.email="rachitdhaka07@gmail.com"
+
+console.log(user.email);
+console.log(user2.email);
+
+
