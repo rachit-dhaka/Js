8e761554b8bda5910a4f381f27ca63c58e0f96bd
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-08-19 17:41:22 +0530

    Basic ho gaya - data type

---
+++ Basic/comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+
+// comparison of null charater 
+console.log("NUll charater comparision ");
+
+console.log(null>0);
+console.log(null==0);
+console.log(null<0);
+console.log(null<=0);
+console.log(null>=0);
+
+
+//  === strict check 
+console.log("Strict check");
+
+console.log("2" === 2);
+console.log(2 === 2);
+
+
+
