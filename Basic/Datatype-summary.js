9547b596a5b4b0be0ee27e355c40ef83ef768862
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-08-19 17:53:01 +0530

    data types summary

---
+++ Basic/Datatype-summary.js
@@ -0,0 +1,37 @@
+// type of datatype
+
+// primitive datatype : 7
+/*
+-Strings 
+-Numbers
+-Boolean
+-NULL
+-Undined
+-Symbol
+-Big int
+
+
+
+Non primitive data type
+array , ojects , functions 
+
+ARRAY : 
+
+ */
+
+
+// array 
+const skills=["C++","js","sql"]
+// objects 
+let myobj={
+ name :"rachit ",
+ age:22,
+}
+
+// fuctions
+const myFunctions=function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof myFunctions);
